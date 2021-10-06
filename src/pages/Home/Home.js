26ae610b3374efc524e51dca5a78b89b6f490207
Kwasek <patryk.kwasek@accenture.com>
2021-10-06 13:40:18 +0200

    Start React Currency Converter project

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Footer } from '../../components/Footer/Footer';
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Currency Converter</h2>
+
+      <Footer />
+    </div>
+  )
+}
